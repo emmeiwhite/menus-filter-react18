674545c850aec402b350585688e757bcab24ab2a
Imran Rafiq Rather <emmeiwhite@gmail.com>
2024-05-11 14:44:46 +0530

    Menus App CSS done!

--- src/Header.jsx
+++ src/Header.jsx
@@ -3,7 +3,7 @@ export default function Header({ menus, handleMenuFilter }) {
   console.log(categories)
 
   return (
-    <div className="">
+    <div className="btn-container">
       {categories.map(category => {
         return (
           <button
